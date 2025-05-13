4137429c95495ee94319732355015aad4a067f8b
Manmit Pal <manmitpal348@gmail.com>
2025-05-13 14:15:40 +0530

    not able to regsiter, might have to migrate to Mongoose for schema

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -3,30 +3,47 @@ import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
-
 export const POST = async (req: Request) => {
-    const { name, email, password } = await req.json();
-    if(!name || !email || !password){
-        return NextResponse.json({message: "Invalid Data"}, {status: 422});
-    }
-    try {
-        await connectDB();
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const user = await prisma.User.create({data: {email, name, password : hashedPassword}})
-        return NextResponse.json({
-            message: "User successfully created",
-            ...user,
-        },{
-            status: 201
-        })
-        /* eslint-disable @typescript-eslint/no-explicit-any */
-    } catch (err: any) {
-        return NextResponse.json({
-            message: "Server Error",
-            ...err,
-        },{status: 500});
-        
-    } finally {
-        await prisma.$disconnect();
-    }
+  const { name, email, password } = await req.json();
+
+  if (!name || !email || !password) {
+    return NextResponse.json({ message: "Invalid Data" }, { status: 422 });
+  }
+
+  try {
+    await connectDB();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          profileUrl: true,
+        },
+      });
+
+    return NextResponse.json(
+      {
+        message: "User successfully created",
+        user,
+      },
+      { status: 201 }
+    );
+  } catch (err: any) {
+    return NextResponse.json(
+      {
+        message: "Server Error",
+        error: err.message,
+        code: err.code,
+      },
+      { status: 500 }
+    );
+  }
 };
