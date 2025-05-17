52f4dec22239d6e7a144fa532ab45be849c88c54
Manmit Pal <manmitpal348@gmail.com>
2025-05-17 12:24:15 +0530

    Getting auth user data on client-side

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+"use client";
+import { useSession } from 'next-auth/react'
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log(session); // Checking for session.user.email etc.
+export default function Home() {
+  // do not use async on client side 
+  // const session = await getServerSession(authOptions);
+  // console.log(session); // Checking for session.user.email etc.
 
+  const { data: session, status } = useSession(); 
+  
+  if (status === "authenticated") {
+    console.log(session?.user); // ✅ CORRECT
+  
   return (
-    <main>
-      <div>Welcome, {session?.user?.email ?? "Guest"}</div>
+    <main className='p-4' >
+      <div className='text-lg font-semibold' >Welcome, {session?.user?.name || "User"}</div>
+      <div>Status: {status}</div>
+      <div className='text-sm text-gray-600' >
+      Session expires at: {new Date(session.expires).toLocaleString()}
+      </div>
     </main>
   );
+
+ }
+ return <div className="p-4">You are not signed in.</div>;
 }
