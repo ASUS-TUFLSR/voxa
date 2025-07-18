d93e4ec9defef85e59b1d0facf6cdbcaad11e442
Manmit Pal <manmitpal348@gmail.com>
2025-07-18 22:12:25 +0530

    Updating/Getting the blogs by ID

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,33 @@
+import { connectDB, generateErrorMessage, generateSuccessMessage } from "@/lib/helpers"
+import prisma from "@/prisma";
+
+
+// Getting the blogs from mongoDB
+export const GET = async ( req: Request ,{ params }: { params: { id: string }}) => {
+    try {
+        const id = params.id;
+        await connectDB();
+        const blog = await prisma.blog.findFirst({where: {id}});
+        return generateSuccessMessage({blog}, 200);
+    } catch (error) {
+        generateErrorMessage({error}, 500);
+    } finally {
+        await prisma.$disconnect();
+    }  
+};
+
+// Updating the blog
+export const PUT = async ( req: Request ,{ params }: { params: { id: string }}) => {
+    try {
+        const { title, description } =  await req.json();
+        if(!title || !description) return generateErrorMessage({reason: "Invalid Data"}, 422)
+        const id = params.id;
+        await connectDB();
+        const blog = await prisma.blog.update({where: {id}, data:{title, description}});
+        return generateSuccessMessage({blog}, 200);
+    } catch (error) {
+        generateErrorMessage({error}, 500);
+    } finally {
+        await prisma.$disconnect();
+    }  
+};
