38fba305dc0b3f738382e7add50fdf7ea689f4e9
Manmit Pal <manmitpal348@gmail.com>
2026-02-14 12:13:37 +0530

    emoji added with some changes

--- app/components/BlogEditor.tsx
+++ app/components/BlogEditor.tsx
@@ -7,24 +7,56 @@ import {
   Image,
   Link,
   QuickToolbar,
+  EmojiPicker,
   Inject,
 } from "@syncfusion/ej2-react-richtexteditor";
 
 interface BlogEditorProps {
-  value: string; // controlled value
-  onChange: (val: string) => void; // callback to parent
+  value: string;
+  onChange: (val: string) => void;
 }
 
 const WriteBlog = ({ value, onChange }: BlogEditorProps) => {
+  const toolbarSettings = {
+    items: [
+      "Bold",
+      "Italic",
+      "Underline",
+      "|",
+      "Formats",
+      "Alignments",
+      "|",
+      "OrderedList",
+      "UnorderedList",
+      "|",
+      "CreateLink",
+      "Image",
+      "|",
+      "EmojiPicker",
+      "|",
+      "SourceCode",
+    ],
+  };
+
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
       <div className="w-full border border-gray-300 rounded-md overflow-hidden shadow-sm">
         <RichTextEditorComponent
           height={400}
-          value={value} // controlled
-          change={(e) => onChange(e.value as string)} // sync with parent
+          value={value}
+          toolbarSettings={toolbarSettings}
+          change={(e) => onChange(e.value as string)}
         >
-          <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+          <Inject
+            services={[
+              HtmlEditor,
+              Toolbar,
+              Image,
+              Link,
+              QuickToolbar,
+              EmojiPicker,
+            ]}
+          />
         </RichTextEditorComponent>
       </div>
     </div>
